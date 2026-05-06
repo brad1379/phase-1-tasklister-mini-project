57676bc1f3c496d3cfcbd35da13547c54164425e
Brad Arnold <brad1379@gmail.com>
2026-05-06 11:20:57 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+  const taskList = document.querySelector("#tasks");
+  const newTask = document.querySelector("#new-task-description");
+
+  console.log(newTask.value);
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    buildToDo(newTask.value);
+
+  });
+
+  function buildToDo(newTask) {
+    const taskItem = document.createElement("li");
+    taskItem.append(newTask)
+    taskList.append(taskItem);
+  };
 });
+
